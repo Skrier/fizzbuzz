0e8de62b3edb3daae8bacb16cc31446b25adfbb6
Skrier <alex.lewis79@yahoo.co.uk>
2014-07-17 13:49:01 +0100

    Initial commit

---
+++ js/application.js
@@ -0,0 +1,54 @@
+$(document).ready(function (){
+    
+    var fizz = "<li> FIZZ </li>";
+	var buzz = "<li> BUZZ </li>";
+	var fizzBuzz = "<li>FIZZBUZZ</li>";
+	var noFizzBuzz = "<li>" + i + "</li>";
+    
+    var fizzbuzz = function (inputNumber){
+	     
+	     var inputNumber = $(".input").val();
+	     
+	        for (var i = 1; i <= inputNumber; i++) {
+	    
+	          if (i % 3 == 0 && i % 5 == 0) {
+	          	$("#result").append(fizzBuzz)
+	          }
+	          else if (i % 3 == 0) {
+	          	$("#result").append(fizz)
+	          }
+	          else if (i % 5 == 0) {
+	          	$("#result").append(buzz)
+	          }
+	          else {$("#result").append(noFizzBuzz)
+	          };
+	                          
+	        };
+
+    };
+
+        $("#inputNumber").mousedown(function () {
+        	fizzbuzz();
+        });
+
+        $(".input").keydown(function (pressenter){
+	        if(pressenter.keyCode == 13) {
+	        fizzbuzz();
+	        };
+        });
+
+        $("#clearResult").mousedown(function () {
+        	$("li").remove();
+        });
+
+
+
+});
+
+
+
+
+
+
+
+   
